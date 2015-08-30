5db355d8dc8a4d06e5d37228b8235e1106472cd0
Ismael <ismael.bernier.public@gmail.com>
2015-08-30 16:27:15 -0400

    [PAWSNCLAWS] remove mainPaws.js file with api key from tracking

--- js/components/paws/mainPaws.js
+++
@@ -1,6 +0,0 @@
-pawsControllers.controller('mainPaws', ['$scope', '$routeParams', '$http', 'Data',
-function($scope, $routeParams, $http, Data) {
-    $scope.data=Data;
-    $scope.data.key='xxxx'
-
-}]);
